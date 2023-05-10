4537757f38cbb22b998d1bd53a7cdb74a8783f12
xCodeMuse <suryapalsingh.inda@gmail.com>
2023-05-10 18:39:47 +0530

    removed dummy model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
 import { StudentsModule } from './students/students.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://Surya:Surya@cluster0.bwfkc.mongodb.net/nestJs?retryWrites=true&w=majority'),
-    CatsModule,
     StudentsModule
   ],
 })
